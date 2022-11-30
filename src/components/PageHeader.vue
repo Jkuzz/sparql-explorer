a2867e14c41e3acc59e144155d49e0b130efc4d0
Jan Kuzelik <honza.ku@gmail.com>
2022-11-30 11:12:11 +0100

    add configs

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <nav class="bg-gray-700 text-gray-200 space-x-4 p-2">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
