956fefeb2bb944ebe62de5b5ec568b4dcbe11b7e
Jan Kuzelik <honza.ku@gmail.com>
2022-12-09 14:09:39 +0100

    Remove aa-import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(autoAnimatePlugin)
 
 app.mount('#app')
