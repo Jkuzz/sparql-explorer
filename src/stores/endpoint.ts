97c86b29a22f0b4383314fc6756ea48a386867b6
Jan Kuzelik <honza.ku@gmail.com>
2023-01-06 11:57:39 +0100

    Return necessary functionality to endpointStore

--- src/stores/endpoint.ts
+++ src/stores/endpoint.ts
@@ -1,9 +1,15 @@
 import { ref, reactive } from 'vue'
 import { defineStore } from 'pinia'
-
-import QueryQueue from '@/stores/queryQueue'
-import { queryClasses } from '@/stores/queryHandler'
+import { makeNodeObject, makeEdgeObject } from '@/stores/queryHandler'
 import type { MarkerType } from '@vue-flow/core'
+import QueryQueue from '@/stores/queryQueue'
+import {
+  getClassesQuery,
+  getClassLinksQuery,
+  getClassPropertiesQuery,
+} from '@/components/force/sparql'
+
+const ATTRIBUTE_MINIMUM_FACTOR = 0.01
 
 export type StoreNode = {
   position: {
@@ -70,8 +76,77 @@ export const useEndpointStore = defineStore('endpoint', () => {
     edges.splice(0, edges.length)
   }
 
-  function query(queryToPerform: string, callback: (data: unknown) => void) {
-    queryQueue.query(queryToPerform, callback)
+  /**
+   * Get the top 10 + offset most numerous classes from the endpoint
+   * @param offset from most numerous class
+   */
+  function queryClasses(offset = 0) {
+    const query = getClassesQuery(offset)
+    queryQueue.query(query, classQueryCallback)
+  }
+
+  /**
+   * Process the classes query response into objects
+   * and perform followup actions and queue followup queries
+   * @param res Classes query response
+   */
+  function classQueryCallback(res: any) {
+    res.results.bindings.forEach((node: any) => {
+      const nodeObject = makeNodeObject(node)
+      if (addNode(nodeObject)) {
+        queryClassEdges(nodeObject)
+        queryClassProperties(nodeObject)
+      }
+    })
+  }
+
+  /**
+   * Get the node's properties from the endpoint
+   * @param newNode for whose properties to query
+   */
+  function queryClassProperties(newNode: StoreNode) {
+    const query = getClassPropertiesQuery(newNode.id)
+    const callback = (res: any) => {
+      res.results?.bindings?.forEach((prop: any) => {
+        if (prop?.property?.value === 'http://www.w3.org/2000/01/rdf-schema#label') {
+          if (!newNode.data.labels) newNode.data.labels = []
+          newNode.data.labels.push(prop)
+        }
+      })
+    }
+    queryQueue.query(query, callback)
+  }
+
+  function queryClassEdges(newNode: StoreNode) {
+    nodes.forEach((n) => {
+      if (n.id === newNode.id) return
+      askEdgeExists(newNode.data.class.value, n.data.class.value, newNode.data.instanceCount.value)
+    })
+  }
+
+  /**
+   * Ask whether there exists a property ?originClass ?property ?targetClass
+   * @param sourceClass URI of origin class
+   * @param targetClass URI of target class
+   * @param originCount instance count of origin
+   */
+  function askEdgeExists(sourceClass: string, targetClass: string, originCount: number) {
+    const linksQuery = getClassLinksQuery(sourceClass, targetClass)
+
+    // Create closure around the callback, binding the class data
+    const callbackFunc = (res: any) => {
+      res.results.bindings
+        .filter(
+          (binding: { instanceCount: { value: number } }) =>
+            binding.instanceCount.value > originCount * ATTRIBUTE_MINIMUM_FACTOR
+        )
+        .forEach((edge: unknown) => {
+          const edgeObject = makeEdgeObject(edge, sourceClass, targetClass)
+          addEdge(edgeObject)
+        })
+    }
+
+    queryQueue.query(linksQuery, callbackFunc)
   }
 
   return {
@@ -79,7 +154,6 @@ export const useEndpointStore = defineStore('endpoint', () => {
     edges,
     addNode,
     addEdge,
-    query,
     renderEdges,
     endpointURL,
     changeEndpoint,
