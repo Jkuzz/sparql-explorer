ae04778f919190617e23f0c0f1ad33b51ac9ca95
Jan Kuzelik <honza.ku@gmail.com>
2023-04-03 18:33:10 +0200

    Added logo to page header non-home views

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -18,21 +18,28 @@ const links = [
 
 <template>
   <header>
-    <nav
-      class="bg-blue-700 text-neutral-300 py-4 px-6 space-x-10 flex flex-row justify-end items-center"
-    >
-      <RouterLink
-        v-for="link in links"
-        :key="link.to"
-        :to="link.to"
-        class="transition-all font-semibold hover:text-neutral-50 group"
-      >
-        {{ link.title }}
-        <div
-          :class="[link.to == router.currentRoute.value.path ? 'w-full' : 'w-0']"
-          class="bg-blue-300 py-[1px] group-hover:w-full transition-all duration-300 ease-out"
-        />
+    <nav class="bg-blue-700 text-neutral-300 py-4 px-6 flex flex-row justify-between items-center">
+      <RouterLink to="/">
+        <span
+          class="font-novem text-xl"
+          v-if="router.currentRoute.value.path != '/'"
+          >Type SPARQ✨</span
+        >
       </RouterLink>
+      <div class="flex flex-row space-x-10">
+        <RouterLink
+          v-for="link in links"
+          :key="link.to"
+          :to="link.to"
+          class="transition-all font-semibold hover:text-neutral-50 group"
+        >
+          {{ link.title }}
+          <div
+            :class="[link.to == router.currentRoute.value.path ? 'w-full' : 'w-0']"
+            class="bg-blue-300 py-[1px] group-hover:w-full transition-all duration-300 ease-out"
+          />
+        </RouterLink>
+      </div>
     </nav>
   </header>
 </template>
